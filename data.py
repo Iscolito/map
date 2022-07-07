967e0a05e257d0c53c105fa5505c90a9fe299e5d
Iscolito <513319484@qq.com>
2022-07-07 22:54:32 +0800

    beta1

--- data.py
+++ data.py
@@ -3,36 +3,76 @@
 # @Author : Iscolito
 # @File : data.py
 # @Software : PyCharm
+
 import pandas as pd
 import streamlit as st
+import plotly.express as px
+import time
 
-# markdown
-st.markdown('Streamlit Demo')
+#读取基本数据集
+df=pd.read_excel("D:\data\list.xls")
+department = df['学校'].unique().tolist()#学校列
+number = df['学生人数'].unique().tolist()#人数列
+position=[[34.15,109.32]]
+for i in range(df.shape[0]):
+    position.append([df.values[i,1],df.values[i,2]])
 
-# 设置网页标题
-st.title('赵大中我爱你')
+st.markdown('书沁校园，情暖蓝田')
+#页眉
+st.title('蓝田教育调研数据统计')
 
-# 展示一级标题
-st.header('1. 安装')
+#下拉框
+st.code('单个学校查询')
+st.markdown('(请在左侧下拉框中选择学校)')
+product_list = df['学校'].unique()
 
-st.text('和安装其他包一样，安装 streamlit 非常简单，一条命令即可')
-code1 = '''pip3 install streamlit'''
-st.code(code1, language='bash')
+product_type = st.sidebar.selectbox(
+    "请选择学校",
+    product_list
+)
+part_df = df[(df['学校'] == product_type)]
+if product_type!='无':
+    st.write(part_df)
+
+#数据筛选
+number_selection = st.slider('学生人数:',min_value=min(number),max_value=max(number),value=(min(number), max(number)))
+st.text('(根据年龄筛选信息拖动数据条，以显示不同条件下的统计图)')
+department_selection = st.multiselect('数据选项:',department,default=department)
+
+#年龄分布图
+st.header('1.年龄分布图')
+mask = (df['学生人数'].between(*number_selection)) & (df['学校'].isin(department_selection))
+number_of_result1 = df[mask].shape[0]
+st.markdown(f'*有效数据: {number_of_result1}*')
+selected={'学校':df[mask]['学校'],'学生人数':df[mask]['学生人数']}
+df_grouped1=pd.DataFrame(selected)
+bar_chart = px.bar(selected, x='学校',y='学生人数',color_discrete_sequence=['#F63366']*len(df_grouped1),template='plotly_white')
+st.plotly_chart(bar_chart)
+df_grouped2=pd.DataFrame({'学校':df[mask]['学校'],'学生人数':df[mask]['学生人数'],'基本信息':df[mask]['基本信息']})
+
+# 统计表
+st.header('2.统计表')
+st.write(df_grouped2)
+
+#地图
+st.header('3.地图')
+st.text('取决于您的网速，加载可能需要时间')
+
+part_df=pd.DataFrame(
+    position,
+    columns=['lat', 'lon'])
+st.map(part_df)
+#进度条显示
+latest_iteration = st.empty()
+bar = st.progress(0)
+for i in range(100):
+  # Update the progress bar with each iteration.
+  latest_iteration.text(f'当前加载进度 {i+1}')
+  bar.progress(i + 1)
+  time.sleep(0.1)
 
 
-# 展示一级标题
-st.header('2. 使用')
 
-# 展示二级标题
-st.subheader('2.1 生成 Markdown 文档')
-
-# 纯文本
-st.text('导入 streamlit 后，就可以直接使用 st.markdown() 初始化')
-
-# 展示代码，有高亮效果
-code2 = '''import streamlit as st
-st.markdown('Streamlit Demo')'''
-st.code(code2, language='python')
 
 
 
