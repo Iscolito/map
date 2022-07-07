cef3415665582612d42f70778750b8a3add31197
Iscolito <513319484@qq.com>
2022-07-07 22:54:46 +0800

    beta1

---
+++ test.py
@@ -0,0 +1,12 @@
+# -*- coding = utf-8 -*-
+# @Time : 2022/7/7 20:34
+# @Author : Iscolito
+# @File : test.py
+# @Software : PyCharm
+list1 = ['physics', 'chemistry', 1997, 2000]
+
+list2 = [1, 2, 3, 4, 5, 6, 7 ]
+
+print("list1[0]: ", list1[0])
+
+print("list2[1:5]: ", list2[1:5])
